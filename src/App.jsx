f8a9fd4b26cf88b364090d7d2a38bd875b0ba927
Fernando <fernandoale00@hotmail.com>
2024-09-17 13:05:11 -0300

    'localStorageYComponetizado'

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,35 @@
-import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { useEffect, useState } from 'react'
 import './App.css'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-
-
-const Square = ({ children, isSelected ,updateBoard, index }) =>
-  {
-  
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-  
-  return(
-    <div onClick={handleClick} className={className}>
-    {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
-const checkWinner = (boardToCheck) => {
-  for (const combo of WINNER_COMBOS) {
-    const [a,b,c] = combo
-    if (
-      boardToCheck[a] && 
-      boardToCheck[a] === boardToCheck[b] &&
-      boardToCheck[a] === boardToCheck[c]
-    ) {
-      return boardToCheck[a]
-    }
-  }
-  return null
-}
-
-
+import { Square } from "./components/Square.jsx"
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { TURNS } from "./constants.js"
+import { checkEndGame, checkWinner } from './logic/board.js'
+import { resetGameStorage, saveGameToStorage } from './logic/index.js'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   // null no hay ganador, false empate
   const [winner, setWinner] = useState(null)
 
- 
+    
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
 
+    resetGameStorage()
+  }
+  
   const updateBoard = (index) => {
     
     if(board[index] || winner) return
@@ -69,20 +39,24 @@ function App() {
     setBoard(newBoard)
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
 
+    setTurn(newTurn)
+    
+    saveGameToStorage({board: newBoard, turn:newTurn})
+    
     const newWinner = checkWinner(newBoard)
     if (newWinner){
+      confetti()
       setWinner(newWinner) // recordar que los estados son asincronos
-    } 
-      
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
+    }
   }
 
-  const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
-  }
+  useEffect(() => {
+
+  },[winner])
+
 
   return (
     <main className="board">
@@ -90,14 +64,14 @@ function App() {
       <button onClick={resetGame}>Reset del Juego</button>
       <section className="game">
         {
-          board.map((_, index) =>{
+          board.map((square, index) =>{
             return (
               <Square 
               key={index} 
               index={index} 
               updateBoard={updateBoard}
               >
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -112,24 +86,7 @@ function App() {
         </Square>
       </section>
       <section>
-        {
-          winner != null && (
-            <section className="winner">
-              <div className="text">
-                <h2>{
-                  winner === false? 'empate' : 'Gano: '
-                }
-                </h2>
-                <header className="win">
-                  {winner && <Square>{winner}</Square>}
-                </header>
-                <footer>
-                  <button onClick={resetGame}>Empezar de nuevo</button>
-                </footer>
-              </div>
-            </section>
-          )
-        }
+        <WinnerModal resetGame={resetGame} winner={winner} />
       </section>
     </main>
   )
